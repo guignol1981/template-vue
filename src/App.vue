a4f2b9baec72c34db84ac8e61a393bf4a09713c6
Vincent Guillemette <vincentguillemette1981@gmail.com>
2024-03-05 06:15:40 -0500

    first

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,28 @@
+<template>
+    <PhaserGame ref="phaserRef" @current-active-scene="currentScene" />
+    <div>
+        <div>
+            <button class="button" @click="changeScene">Change Scene</button>
+        </div>
+        <div>
+            <button
+                :disabled="canMoveSprite"
+                class="button"
+                @click="moveSprite"
+            >
+                Toggle Movement
+            </button>
+        </div>
+        <div class="spritePosition">
+            Sprite Position:
+            <pre>{{ spritePosition }}</pre>
+        </div>
+        <div>
+            <button class="button" @click="addSprite">Add New Sprite</button>
+        </div>
+    </div>
+</template>
+
 <script setup>
 import Phaser from 'phaser';
 import { ref, toRaw } from 'vue';
@@ -11,39 +36,29 @@ const phaserRef = ref();
 const spritePosition = ref({ x: 0, y: 0 });
 
 const changeScene = () => {
-
     const scene = toRaw(phaserRef.value.scene);
 
-    if (scene)
-    {
+    if (scene) {
         //  Call the changeScene method defined in the `MainMenu`, `Game` and `GameOver` Scenes
         scene.changeScene();
     }
-
-}
+};
 
 const moveSprite = () => {
-
     const scene = toRaw(phaserRef.value.scene);
 
-    if (scene)
-    {
+    if (scene) {
         //  Call the `moveLogo` method in the `MainMenu` Scene and capture the sprite position
         scene.moveLogo(({ x, y }) => {
-
             spritePosition.value = { x, y };
-
         });
     }
-
-}
+};
 
 const addSprite = () => {
-
     const scene = toRaw(phaserRef.value.scene);
 
-    if (scene)
-    {
+    if (scene) {
         //  Add a new sprite to the current scene at a random position
         const x = Phaser.Math.Between(64, scene.scale.width - 64);
         const y = Phaser.Math.Between(64, scene.scale.height - 64);
@@ -59,34 +74,13 @@ const addSprite = () => {
             duration: 500 + Math.random() * 1000,
             alpha: 0,
             yoyo: true,
-            repeat: -1
+            repeat: -1,
         });
     }
-
-}
+};
 
 //  This event is emitted from the PhaserGame component:
 const currentScene = (scene) => {
-
-    canMoveSprite.value = (scene.scene.key !== 'MainMenu');
-
-}
+    canMoveSprite.value = scene.scene.key !== 'MainMenu';
+};
 </script>
-
-<template>
-    <PhaserGame ref="phaserRef" @current-active-scene="currentScene" />
-    <div>
-        <div>
-            <button class="button" @click="changeScene">Change Scene</button>
-        </div>
-        <div>
-            <button :disabled="canMoveSprite" class="button" @click="moveSprite">Toggle Movement</button>
-        </div>
-        <div class="spritePosition">Sprite Position:
-            <pre>{{ spritePosition }}</pre>
-        </div>
-        <div>
-            <button class="button" @click="addSprite">Add New Sprite</button>
-        </div>
-    </div>
-</template>
